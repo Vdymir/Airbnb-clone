9f2e1835748d6c4d781047eaa594d99902c9f527
vdymir <vladistar200302@gmail.com>
2024-01-31 10:26:16 -0500

    Initial Commit

---
+++ Airbnb-clone/Airbnb_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Airbnb_cloneApp.swift
+//  Airbnb-clone
+//
+//  Created by Vladimir Castañeda on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Airbnb_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Airbnb-clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Airbnb-clone
+//
+//  Created by Vladimir Castañeda on 31/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
